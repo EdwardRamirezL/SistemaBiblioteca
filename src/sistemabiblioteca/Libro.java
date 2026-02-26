c429b9c7136ea7c77174750a9e088ee7b5036906
Eduard Ramirez <ramirezedward535@gmail.com>
2026-02-26 18:20:30 -0500

    feat: Agregar constructor y encapsulamiento a clase Libro.

--- src/sistemabiblioteca/Libro.java
+++ src/sistemabiblioteca/Libro.java
@@ -1,13 +1,93 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sistemabiblioteca;
 
-/**
- *
- * @author Eduard Ramirez
- */
 public class Libro {
+    private String isbn,titulo,autor,genero;
+    private int anioPublicacion;
+    private boolean disponible;
+
+    /**
+     * @return the isbn
+     */
+    public String getIsbn() {
+        return isbn;
+    }
+
+    /**
+     * @param isbn the isbn to set
+     */
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the autor
+     */
+    public String getAutor() {
+        return autor;
+    }
+
+    /**
+     * @param autor the autor to set
+     */
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    /**
+     * @return the genero
+     */
+    public String getGenero() {
+        return genero;
+    }
+
+    /**
+     * @param genero the genero to set
+     */
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    /**
+     * @return the anioPublicacion
+     */
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    /**
+     * @param anioPublicacion the anioPublicacion to set
+     */
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    /**
+     * @return the disponible
+     */
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    /**
+     * @param disponible the disponible to set
+     */
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
     
 }
